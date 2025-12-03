4212cf6cf80fac5eb45cd0889cf6ef2c8d42b94a
Jashan sidhu <jsidhu55@academic.rrc.ca>
2025-12-03 04:27:16 -0600

    Update lambda_function.py

--- lambda/add_inventory_item/lambda_function.py
+++ lambda/add_inventory_item/lambda_function.py
@@ -1,25 +1,14 @@
 import boto3
 import json
-import ulid
-
-dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table("Inventory")
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Inventory')
 
 def lambda_handler(event, context):
     body = json.loads(event["body"])
 
-    new_item = {
-        "item_id": str(ulid.ULID()),  # generate unique ID
-        "item_location_id": int(body["item_location_id"]),
-        "item_name": body["item_name"],
-        "item_description": body["item_description"],
-        "qty_on_hand": int(body["qty_on_hand"]),
-        "item_price": float(body["item_price"])
-    }
-
-    table.put_item(Item=new_item)
+    table.put_item(Item=body)
 
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "Item added", "item": new_item})
+        "body": "Item added successfully"
     }
